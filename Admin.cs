eb9b8246d102e63ee2e7c88e821e0e6f19355eec
ALJI-1 <albin.ingelsson1@gmail.com>
2024-10-24 15:05:33 +0200

    Ändringar i användare - visa lösenord (inte klar)

--- Admin.cs
+++ Admin.cs
@@ -9,8 +9,11 @@ namespace Inloggningsfunktion_Gruppövning
 {
     public class Admin
     {
+        
         public void MetodAdminMeny()
         {
+
+            
             Console.WriteLine("Välkommen Admin!");
             Console.WriteLine("1. Skapa användare");
             Console.WriteLine("2. Inställningar");
@@ -23,7 +26,7 @@ namespace Inloggningsfunktion_Gruppövning
                     Console.WriteLine("Skapa användare");
                     break;
                 case "2":
-                    Console.WriteLine("Inställningar\n1. Byt till slumpmässig färg på texten\n2.  Ändra konsolfönstrets titel");
+                    Console.WriteLine("Inställningar\n1. Byt till slumpmässig färg på texten\n2. Ändra konsolfönstrets titel");
                     int choice = int.Parse(Console.ReadLine());
                     if (choice == 1)
                     {
@@ -46,6 +49,19 @@ namespace Inloggningsfunktion_Gruppövning
                     break;
             }
         }
+        public void AddUser()
+{
+        Console.WriteLine("Ange användarnamn: ");
+        string userName = Console.ReadLine();
+        Console.WriteLine("Ange lösenord: ");
+        int password = int.Parse(Console.ReadLine());
+
+        Security user = new Security(password, userName);
+        Program.Användare.Add(user);
+        Console.WriteLine("Användare skapad!");
+
+        MetodAdminMeny();
+}
         public static void ChangeFontRGB()
         {
             Random random = new Random();
@@ -60,11 +76,6 @@ namespace Inloggningsfunktion_Gruppövning
             Console.ForegroundColor = randomColor;
             Console.WriteLine($"Färgen ändrad till: {randomColor}. Tryck enter.");
             Console.ReadLine();
-            
-
-            
-
-
         }
 
     }
